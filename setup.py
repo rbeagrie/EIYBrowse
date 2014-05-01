660409b1cc6ad8caef94e0c63e6e798615fa9a35
Rob Beagrie <rob@beagrie.com>
2014-05-01 14:58:53 +0100

    Added setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "browser",
+    version = "0.0.1",
+    author = "Rob Beagrie",
+    author_email = "rob@beagrie.com",
+    description = ("A genome browser written in python."),
+    packages=['browser'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+    ],
+)
