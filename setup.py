5132406b576692214b817c08c3f0f760ffa172c2
Rob Beagrie <rob@beagrie.com>
2014-06-02 14:54:06 +0100

    Spelling mistake in setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "EIYbrowse",
+    name = "EIYBrowse",
     version = "0.0.1",
     author = "Rob Beagrie",
     author_email = "rob@beagrie.com",
     description = ("A genome browser written in python."),
-    packages=['EIYbrowse'],
+    packages=['EIYBrowse'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
