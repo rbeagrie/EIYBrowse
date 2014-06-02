fe7755414bf99bb3b380ec549af0b5bdd74229e4
Rob Beagrie <rob@beagrie.com>
2014-06-02 14:54:29 +0100

    Added a panel for placing a scale bar

---
+++ EIYBrowse/utils.py
@@ -0,0 +1,15 @@
+
+def format_genomic_distance(distance, precision=1):
+    """Turn an integer genomic distance into a pretty string"""
+
+    formatting_string = '{{0:.{0}f}}'.format(precision)
+    
+    if distance < 1000:
+        return '{0:d}bp'.format(distance)
+    elif distance < 1000000:
+        fmt_string = formatting_string + 'kb'
+        return fmt_string.format(float(distance) / 1000)
+    else:
+        fmt_string = formatting_string + 'Mb'
+        return fmt_string.format(float(distance) / 1000000)
+
