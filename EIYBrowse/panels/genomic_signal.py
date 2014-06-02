5c28a81caff22cae4757199b5224d6ac4358059a
Rob Beagrie <rob@beagrie.com>
2014-06-02 14:53:48 +0100

    Panels can be named, and now return a reference to the plotted data

--- EIYBrowse/panels/genomic_signal.py
+++ EIYBrowse/panels/genomic_signal.py
@@ -3,10 +3,10 @@ from metaseq._genomic_signal import genomic_signal
 
 class GenomicSignalPanel(Panel):
     """Panel for displaying a continuous signal (e.g. ChIP-seq) accross a genomic region"""
-    def __init__(self, gsignal_path, gsignal_type, bins=800, color='#377eb8'):
+    def __init__(self, gsignal_path, gsignal_type, name=None, bins=800, color='#377eb8'):
         super(GenomicSignalPanel, self).__init__()
 
-        self.bins, self.color = bins, color
+        self.name, self.bins, self.color = name, bins, color
         
         self.genomic_signal = genomic_signal(gsignal_path, gsignal_type)
 
@@ -16,8 +16,12 @@ class GenomicSignalPanel(Panel):
 
     def _plot(self, ax, feature):
 
-        ax.axis('off')
+        ax.set_axis_off()
+
         sig_x, sig_y = self.genomic_signal.local_coverage(feature, bins=self.bins)
         patches = ax.fill_between(sig_x, sig_y, color=self.color)
         ax.set_xlim(feature.start, feature.stop)
-        return patches
+
+        return { 'patches' : patches ,
+                 'data' : (sig_x, sig_y),
+               }
