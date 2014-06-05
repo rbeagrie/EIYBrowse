2980bc6305fd026d7011ab2ca226ffb063fb888f
Rob Beagrie <rob@beagrie.com>
2014-06-05 11:52:43 +0100

    Allow import of panels from EIBrowse.panels

---
+++ EIYBrowse/panels/__init__.py
@@ -0,0 +1,7 @@
+from .gam_interactions import InteractionsGamPanel
+from .genes import GenePanel
+from .genomic_signal import GenomicSignalPanel
+from .interactions import InteractionsDbPanel
+from .location import LocationPanel
+from .scale_bar import ScaleBarPanel
+from .interval import GenomicIntervalPanel
