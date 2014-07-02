91b7c4e23a7f12153ef147e671b9dd080bf94070
Rob Beagrie <rob@beagrie.com>
2014-07-02 15:03:44 +0200

    Avoid circular imports with filetype handlers

--- EIYBrowse/filetypes/__init__.py
+++ EIYBrowse/filetypes/__init__.py
@@ -2,13 +2,16 @@ from metaseq._genomic_signal import _registry
 import gffutils
 from pkg_resources import iter_entry_points
 
-defined_filetypes = { ep.name : ep.load() for ep in iter_entry_points('EIYBrowse.filetypes') }
 
-defined_filetypes.update({'gffutils_db': gffutils.FeatureDB})
+def get_file_opener(file_type):
+    defined_filetypes = { ep.name : ep.load() for ep in iter_entry_points('EIYBrowse.filetypes') }
 
-defined_filetypes.update(_registry)
+    defined_filetypes.update({'gffutils_db': gffutils.FeatureDB})
 
-globals().update(defined_filetypes)
+    defined_filetypes.update(_registry)
+
+    return defined_filetypes[file_type]
 
 def open_file(file_path, file_type):
-    return defined_filetypes[file_type](file_path)
+    file_opener = get_file_opener(file_type)
+    return file_opener(file_path)
