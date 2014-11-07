03bf59b65fa5500c26c72ade0aa45e9e22ecc3f3
Rob Beagrie <rob@beagrie.com>
2014-11-07 17:03:56 +0000

    Try to avoid weird stuff happening on readthedocs.

--- EIYBrowse/panels/__init__.py
+++ EIYBrowse/panels/__init__.py
@@ -1,5 +1,12 @@
 from pkg_resources import iter_entry_points
 
-defined_panels = {ep.name: ep.load()
-                  for ep in iter_entry_points('EIYBrowse.panels')}
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if on_rtd:
+    defined_panels = {ep.name: ep.load()
+                      for ep in iter_entry_points('EIYBrowse.panels')}
+else:
+    defined_panels = {}
+
 globals().update(defined_panels)
