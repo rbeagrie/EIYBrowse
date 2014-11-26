0ae55967835d4e4f6f5da4e961361f8db2c9f2b4
Rob Beagrie <rob@beagrie.com>
2014-11-26 12:36:36 +0100

    Don't use gfftools or metaseq if they are not installed

--- EIYBrowse/filetypes/__init__.py
+++ EIYBrowse/filetypes/__init__.py
@@ -1,14 +1,27 @@
-from metaseq._genomic_signal import _registry
-import gffutils
 from pkg_resources import iter_entry_points
 
 
 def get_file_opener(file_type):
-    defined_filetypes = { ep.name : ep.load() for ep in iter_entry_points('EIYBrowse.filetypes') }
+    defined_filetypes = {}
 
-    defined_filetypes.update({'gffutils_db': gffutils.FeatureDB})
+    for ep in iter_entry_points('EIYBrowse.filetypes'):
 
-    defined_filetypes.update(_registry)
+        defined_filetypes[ep.name] = ep.load()
+
+    try:
+        import gffutils
+        defined_filetypes.update({'gffutils_db': gffutils.FeatureDB})
+
+    except ImportError:
+        pass
+
+    try:
+        from metaseq._genomic_signal import _registry
+        defined_filetypes.update(_registry)
+
+    except ImportError:
+        pass
+
 
     return defined_filetypes[file_type]
 
